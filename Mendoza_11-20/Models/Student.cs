6312818a7156aa352d15c865765c19e1b5ec5bff
Tyler <159589919+xanafalgue9@users.noreply.github.com>
2024-11-20 13:44:23 -0600

    new class Student

---
+++ Mendoza_11-20/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Mendoza_11_20.Models
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
